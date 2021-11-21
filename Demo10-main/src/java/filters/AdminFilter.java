af3984e562c3b6f6beb5aaa099ee24933a6a905b
847133 <847133@SAIT227599IT.ACDM.DS.SAIT.CA>
2021-11-21 12:42:22 -0700

    This is lab 10 for CPRG 352. followed the instructions as best as i could. Done most if not all of the project and ill just do more commits if there are bugs in the system

---
+++ Demo10-main/src/java/filters/AdminFilter.java
@@ -0,0 +1,58 @@
+
+package filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
+import services.AccountService;
+
+/**
+ *
+ * @author 847133
+ */
+public class AdminFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+         //any code before chain.doFilter will be executed before the servlet
+        
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpSession session = httpRequest.getSession();
+        int role = (int) session.getAttribute("role");
+        
+        
+        
+        if (role == 2) {
+           HttpServletResponse httpResponse = (HttpServletResponse)response;
+            httpResponse.sendRedirect("login");
+            return;
+        }
+        //this will either call upon the next filter in the chain,
+        // or it will load the requested servlet
+        chain.doFilter(request, response);
+        
+        //any code after chain.doFilter will be executed after the servlet
+        
+    }
+    
+    
+    
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+    @Override
+    public void destroy() {
+    }
+    
+   
+}
